1d0eb4552bcdf612b6eb8489415e8b89afcfb4b1
developerfahad1 <fahadgraphic.pk>
2024-04-20 11:21:43 +0500

    app.js and index.html file created

---
+++ app.js
@@ -0,0 +1,41 @@
+// console.log('hello world');
+
+// toLowerCase
+// toUpperCase
+// length
+// charAt
+// indexOf
+// slice
+// replace
+// replaceall
+// split
+
+const username = 'muhammad fahad'
+// console.log(username.toUpperCase());
+// console.log(username.toUpperCase());
+// console.log(username.length);
+// console.log(username[2]);
+// console.log(username.charAt(3));
+// console.log(username.split('a'));
+
+const fruit = ['apple' , 'orange' , 'mango' , 'nashpate' , 'grapes']
+
+// console.log(fruit.indexOf('nashpate'));
+// console.log(fruit.slice(1 , 4));
+// console.log(fruit.includes('nashpate'));
+
+let message = "Ali and Sami are best and friends. They play cricket Ali and Sami and football Ali and Sami together."
+
+// console.log(message.replace('Ali' , 'Fahad'));
+// console.log(message.replaceAll('Ali' , 'Muhammad Fahad'));
+
+
+const city = prompt ('which city do you live');
+
+if(city.toLowerCase() === 'karachi'){
+    console.log('you live in the safest city');
+}else{
+    console.log('zindagi jhand hai');
+}
+
+
